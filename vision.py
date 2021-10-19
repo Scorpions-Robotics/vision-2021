2d7383864bd36d904e2bbb8ff4291f624dc45e12
egeakman <egeakmanegeakman@hotmail.com>
2021-10-19 18:41:38 +0300

    Create pixel-calculator.

--- vision.py
+++ vision.py
@@ -2,7 +2,6 @@ from networktables import NetworkTables
 import numpy as np
 import cv2
 import imutils
-import time
 import zmq
 
 
@@ -24,18 +23,7 @@ context = zmq.Context()
 footage_socket = context.socket(zmq.PUB)
 footage_socket.connect("tcp://10.7.17.117:5555")
 
-while True:
-
-    (
-        grabbed,
-        frame,
-    ) = camera.read()
-
-    try:
-
-        frame = imutils.rotate(frame, angle=0)
-
-        def white_balance(frame):
+def white_balance(frame):
             result = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
             avg_a = np.average(result[:, :, 1])
             avg_b = np.average(result[:, :, 2])
@@ -48,14 +36,34 @@ while True:
             result = cv2.cvtColor(result, cv2.COLOR_LAB2BGR)
             return result
 
+def calibrate():
+    KNOWN_WIDTH = 39
+    KNOWN_PIXEL_WIDTH = 
+    KNOWN_DISTANCE = 
+    FOCAL_LENGTH = (KNOWN_PIXEL_WIDTH * KNOWN_DISTANCE) / KNOWN_WIDTH
+
+def current_distance():
+
+
+while True:
+
+    (
+        grabbed,
+        frame,
+    ) = camera.read()
+
+    try:
+
+        frame = imutils.rotate(frame, angle=0)
+
         result = white_balance(frame)
 
         gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
-        faces = hoop_classifier.detectMultiScale(
+        hoops = hoop_classifier.detectMultiScale(
             gray, scaleFactor=1.2, minNeighbors=5, minSize=(20, 20)
         )
 
-        for (x, y, w, h) in faces:
+        for (x, y, w, h) in hoops:
             cv2.rectangle(result, (x, y), (x + w, y + h), (255, 0, 0), 2)
             roi_gray = gray[y : y + h, x : x + w]
             roi_color = result[y : y + h, x : x + w]
@@ -70,7 +78,7 @@ while True:
         encoded, buffer = cv2.imencode(".jpg", result)
         footage_socket.send(buffer)
 
-        # TODO: Remove lines #64-68 before deploying to Jetson
+        # TODO: Remove below lines (until break (included)) lines before deploying to Jetson
         cv2.imshow("video", result)
         k = cv2.waitKey(30) & 0xFF
         if k == 27:  # press 'ESC' to quit
