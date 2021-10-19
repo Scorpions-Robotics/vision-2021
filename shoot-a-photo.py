5d696d175eb36dd4de90510a18dbd7590d496752
egeakman <egeakmanegeakman@hotmail.com>
2021-10-20 00:29:43 +0300

    Improve photo shooting.

--- shoot-a-photo.py
+++ shoot-a-photo.py
@@ -5,10 +5,11 @@ cap.set(15, -9)
 ret, frame = cap.read()
 
 while True:
-    cv2.imshow("img1", frame)
-    if cv2.waitKey(1) & 0xFF == ord("y"):
+    cv2.imshow("video", frame)
+    k = cv2.waitKey(1) & 0xFF
+    if k == 89:  # press 'y' to shoot
         cv2.imwrite("images/ref-pic.jpeg", frame)
-        cv2.destroyAllWindows()
         break
 
+cv2.destroyAllWindows()
 cap.release()
