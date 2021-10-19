d81216555c9bfeb5bf4b58a9d52af965efba4647
egeakman <egeakmanegeakman@hotmail.com>
2021-10-19 13:01:00 +0300

    Fix attribute error.

--- vision.py
+++ vision.py
@@ -8,8 +8,8 @@ NetworkTables.initialize(server="roborio-7672-frc.local")
 table = NetworkTables.getTable("Vision")
 
 camera = cv2.VideoCapture(0)
-# camera.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
-camera.set(15, -8)
+camera.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+# camera.set(15, -8)
 
 x = 0
 y = 0
@@ -25,44 +25,49 @@ while True:
         frame,
     ) = camera.read()
 
-    frame = imutils.rotate(frame, angle=0)
+    try:
 
-    def white_balance(frame):
-        result = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
-        avg_a = np.average(result[:, :, 1])
-        avg_b = np.average(result[:, :, 2])
-        result[:, :, 1] = result[:, :, 1] - (
-            (avg_a - 128) * (result[:, :, 0] / 255.0) * 1.1
+        frame = imutils.rotate(frame, angle=0)
+
+        def white_balance(frame):
+            result = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+            avg_a = np.average(result[:, :, 1])
+            avg_b = np.average(result[:, :, 2])
+            result[:, :, 1] = result[:, :, 1] - (
+                (avg_a - 128) * (result[:, :, 0] / 255.0) * 1.1
+            )
+            result[:, :, 2] = result[:, :, 2] - (
+                (avg_b - 128) * (result[:, :, 0] / 255.0) * 1.1
+            )
+            result = cv2.cvtColor(result, cv2.COLOR_LAB2BGR)
+            return result
+
+        result = white_balance(frame)
+
+        gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+        faces = hoop_classifier.detectMultiScale(
+            gray, scaleFactor=1.2, minNeighbors=5, minSize=(20, 20)
         )
-        result[:, :, 2] = result[:, :, 2] - (
-            (avg_b - 128) * (result[:, :, 0] / 255.0) * 1.1
-        )
-        result = cv2.cvtColor(result, cv2.COLOR_LAB2BGR)
-        return result
 
-    result = white_balance(frame)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(result, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            roi_gray = gray[y : y + h, x : x + w]
+            roi_color = result[y : y + h, x : x + w]
 
-    gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
-    faces = hoop_classifier.detectMultiScale(
-        gray, scaleFactor=1.2, minNeighbors=5, minSize=(20, 20)
-    )
+        print(x, y, w, h)
 
-    for (x, y, w, h) in faces:
-        cv2.rectangle(result, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        roi_gray = gray[y : y + h, x : x + w]
-        roi_color = result[y : y + h, x : x + w]
+        table.putNumber("X", x)
+        table.putNumber("Y", y)
+        table.putNumber("W", w)
+        table.putNumber("H", h)
 
-    print(x, y, w, h)
+        cv2.imshow("video", result)
+        k = cv2.waitKey(30) & 0xFF
+        if k == 27:  # press 'ESC' to quit
+            break
 
-    table.putNumber("X", x)
-    table.putNumber("Y", y)
-    table.putNumber("W", w)
-    table.putNumber("H", h)
-
-    cv2.imshow("video", result)
-    k = cv2.waitKey(30) & 0xFF
-    if k == 27:  # press 'ESC' to quit
-        break
+    except AttributeError as e:
+        print(e)
 
 camera.release()
 cv2.destroyAllWindows()
