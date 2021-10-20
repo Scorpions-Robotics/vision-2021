14daa5060e121962a3bb6be58b5ee92fbfb346b1
Arda Sak <ardasak434@gmail.com>
2021-10-20 19:05:47 +0300

    update

--- shoot-a-photo.py
+++ shoot-a-photo.py
@@ -1,15 +1,13 @@
 import cv2
-
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(15, -9)
 ret, frame = cap.read()
 
 while True:
-    cv2.imshow("video", frame)
-    k = cv2.waitKey(1) & 0xFF
-    if k == 89:  # press 'y' to shoot
+    cv2.imshow("img1", frame)
+    if cv2.waitKey(1) & 0xFF == ord("y"):
         cv2.imwrite("images/ref-pic.jpeg", frame)
+        cv2.destroyAllWindows()
         break
 
-cv2.destroyAllWindows()
 cap.release()
