2253c0095ffee4be1169171a44a6644cda747e15
Arda Sak <ardasak434@gmail.com>
2021-11-07 17:23:06 +0300

    Pulling data from network tables

--- misc/test/test_networktables.py
+++ misc/test/test_networktables.py
@@ -1,9 +1,15 @@
+
 from networktables import NetworkTables
 
-
-NetworkTables.initialize(server="roborio-7672-frc.local")
-table = NetworkTables.getTable("vision")
+NetworkTables.initialize("roborio-7672-frc.local")
+nt = NetworkTables.getTable("vision")
 
 while True:
-    x = table.getNumber("X", 0)
-    print(x)
+    print("X", nt.getNumber("X", -1))
+    print("Y", nt.getNumber("Y", -1))
+    print("W", nt.getNumber("W", -1))
+    print("H", nt.getNumber("H", -1))
+    print("D", nt.getNumber("D", -1))
+    print("B", nt.getNumber("B", -1))
+    print("R", nt.getNumber("R", -1))
+    
